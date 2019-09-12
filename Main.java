3ce512e206daaaf6ea6b0ec99be5c98ff92500bd
Patrick O'shea <paoshea@verdi.eecs.csuohio.edu>
2019-09-12 16:19:08 -0400

    Wrote a command line parser

--- Main.java
+++ Main.java
@@ -1,5 +1,34 @@
+import java.io.File;
+
 public class Main {
+	enum Mode = {
+		DFS, BFS
+	}
+	private int destination;
+	private Mode searchType;
+	private bool extended;
+	private String fileName;
+
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		extended = false;
+		for (int i =0; i< args.length;i++) {
+			if (args[i] == "-dest") {
+				destination = args[i+1];
+				i++;
+			}
+			if (args[i] == "-dfs") {
+				searchType = Mode.DFS;
+			}
+			if (args[i] == "-bfs") {
+				searchType = Mode.BFS;
+			}
+			if (args[i] == "-e") {
+				extended = true;	
+			}
+			else {
+				fileName = args[i];
+			}
+		}
+
 	}
 }
